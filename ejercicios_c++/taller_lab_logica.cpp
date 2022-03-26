d2d8b61f1b3b5496795e46887090451f24384794
Jerdirlson Santamaria <jerdilson@outlook.es>
2022-03-25 22:59:01 -0500

    modificación

--- ejercicios_c++/taller_lab_logica.cpp
+++ ejercicios_c++/taller_lab_logica.cpp
@@ -59,7 +59,7 @@ Tenga presente que cada artículo tiene un valor diferente.
 
 main()
 {   
-    int num_cam, i, cont_cam, prec_cam, ttl_prc_cam;
+    int num_cam, i, cont_cam, prec_cam, ttl_prc_cam, num_pant, prec_pant, ttl_prc_pant, cont_pant, num_zap, prec_zap, ttl_prc_zap, cont_zap,prec_cha, ttl_prc_cha, cont_cha, num_cha, num_lib, cont_lib, opt, val_lib1, ttl_val_lib1, val_lib2, ttl_val_lib2, val_lib3, ttl_val_lib3,val_lib4, ttl_val_lib4, cont_lib, ttl_libros, total;
 
     cont_cam = 0;
     ttl_prc_cam = 0;
@@ -79,6 +79,103 @@ main()
     while (cont_cam < num_cam);
 
     cout<<"\nEl precio total de las camisetas es: "<<ttl_prc_cam;
+    
+    cout<<"Ingrese la cantidad de pantalones que compro: ";
+        cin>>num_pant;
+
+    do
+    {
+        cout<<"\nIngrese el precio de los pantalones: ";
+        cin>>prec_pant;
+
+        ttl_prc_pant = ttl_prc_pant + prec_pant;
+
+        cont_pant++;
+    } while (cont_pant < num_pant);
+
+    cout<<"\n El precio total de los pantalones es: "<<ttl_prc_pant;
+
+    cout<<"\nIngrese la cantidad de pares de zapatos que compro: ";
+        cin>>num_zap;
+
+    do
+    {
+        cout<<"\nIngrese el precio de los pares de zapatos: ";
+        cin>>prec_zap;
+        
+        ttl_prc_zap = ttl_prc_zap + prec_zap;
+
+        cont_zap++;
+    } while (cont_zap < num_zap);
+    
+    cout<<"\n El precio total de los zapatos es: "<<ttl_prc_zap;
+    
+    cout<<"\nIngrese la cantidad de chaquetas que compro: ";
+        cin>>num_cha;
+
+    do
+    {
+        cout<<"\nIngrese el precio de las chaquetas: ";
+        cin>>prec_cha;
+        
+        ttl_prc_cha = ttl_prc_cha + prec_cha;
+
+        cont_cha++;
+    } while (cont_cha < num_cha);
+
+    cout<<"\n El precio total de las chaquetas es: "<<ttl_prc_cha;
+    
+    cout<<"\nIntroduzca cuantos libros compró: ";
+    cin>>num_lib;
+
+    while (cont_lib < num_lib)
+    {
+        cout<<"\nElija que tipo de libro compró: ";
+        cout<<"\n1.)Programacón.";
+        cout<<"\n2.)Innovación.";
+        cout<<"\n3.)Emprendimiento.";
+        cout<<"\n4.)Lógica de programación.";
+        cin>>opt;
+        switch (opt)
+        {
+        case 1:
+            cout<<"Ingrese el valor del libro de programación: ";
+            cin>>val_lib1;
+
+            ttl_val_lib1 = ttl_val_lib1 + val_lib1;
+            break;
+        case 2:
+            cout<<"Ingrese el valor del libro de innovación: ";
+            cin>>val_lib2;
+
+            ttl_val_lib2 = ttl_val_lib2 + val_lib2
+        
+        case 3:
+            cout<<"Ingrese el valor del libro de emprendimiento: ";
+            cin>>val_lib3;
+
+            ttl_val_lib3 = ttl_val_lib3 + val_lib3;
+
+        case 4: 
+            cout<<"Ingrese el valor del libro de lógica de programación: ";
+            cin>>val_lib4;
+
+            ttl_val_lib4 = ttl_val_lib4 + val_lib4;
+
+        default:
+            cout<<"No selecciono una opción valida";
+            break;
+        }
+        
+        cont_lib++;
+    }
+
+    ttl_libros = ttl_val_lib1 +ttl_val_lib2 + ttl_val_lib3 + ttl_val_lib4;
+
+    total = ttl_prc_cam + ttl_prc_pant + ttl_prc_zap + ttl_prc_cha + ttl_libros;
+
+    cout<<"El costo total de todas las cosas es: "<<total;
+    
     
 
 }
