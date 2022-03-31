17bce0af8bc68914a7c93b1d2e4f8c8536a52cf8
Jerdirlson Santamaria <jerdilson@outlook.es>
2022-03-31 12:24:45 -0500

    Se arreglo, ejercicio definitivo

--- ejercicios_c++/secuencia_positivos_negativos.cpp
+++ ejercicios_c++/secuencia_positivos_negativos.cpp
@@ -4,7 +4,10 @@ using namespace std;
 
 main()
 {   
-    int lim_inf, lim_sup, r, i, t;
+    int lim_inf, lim_sup, r, i, t, j, otro;
+
+    do
+    {
 
     cout<<"Ingrese limite inferior: ";
     cin>>lim_inf;
@@ -13,21 +16,28 @@ main()
     cin>>lim_sup;
 
     r = lim_inf;
+    j = 0;
 
     for ( i = lim_inf + 1; i <= lim_sup; i++)
     {
-        if (i % 2 == 0)
-        {
-            t = -1;
-        }
-        else
+
+        if (j % 2 == 0)
         {
             t = 1;
         }
+        else
+        {
+            t = -1;
+        }
         
-        r = r +(t * i);
+        r = r+ (t * i);
+
+        j++;
 
         cout<<"\nEsto es r: "<<r;
     }
+        cout<<"\nIngrese 1 si quiere repetir el proceso: ";
+        cin>> otro;
+    } while (otro == 1);
      
 }
